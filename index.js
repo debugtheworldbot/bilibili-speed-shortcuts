cf1f6768bd1602c24c18b51f8e5e11966d92cc5f
tian <istiancz@gmail.com>
2023-09-28 15:15:43 +0800

    chore: init

---
+++ index.js
@@ -0,0 +1,91 @@
+// ==UserScript==
+// @name         bilibili 倍速控制
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  使用快捷键控制bilibili的倍速选择
+// @author       pipizhu
+// @match        http*://www.bilibili.com/video/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// @license 		 MIT
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  document.body.onload = init;
+})();
+
+let speedOptions = [];
+function addActionBtns() {
+  const videoArea = document.querySelector(".bpx-player-primary-area");
+  const button = document.createElement("button");
+  button.innerHTML = "倍速";
+  button.style = "cursor: pointer;position: absolute;left: 0;bottom: 0;";
+  videoArea.appendChild(button);
+}
+
+async function init() {
+  try {
+    await delay(1000);
+    speedOptions = await initKeyElems();
+    const currentSpeed = speedOptions.find((item) =>
+      item.className.includes("bpx-state-active"),
+    ).dataset.value;
+
+    bindKeys(currentSpeed);
+  } catch (e) {
+    console.log("error", e);
+  }
+}
+
+function bindKeys(currentSpeed) {
+  const speedList = [0.5, 0.75, 1, 1.25, 1.5, 2];
+  let currentIndex = speedList.indexOf(currentSpeed);
+  document.addEventListener("keydown", (e) => {
+    console.log("keydown", e);
+    if (e.key === "+") {
+      currentIndex =
+        currentIndex >= speedList.length - 1 ? currentIndex : currentIndex + 1;
+    } else if (e.key === "-") {
+      currentIndex = currentIndex <= 0 ? currentIndex : currentIndex - 1;
+    }
+    const speed = speedList[currentIndex];
+    setSpeed(speed);
+  });
+}
+
+function setSpeed(speed) {
+  console.log("setSpeed", speed);
+  speedOptions.find((item) => item.dataset.value === speed.toString()).click();
+}
+
+async function initKeyElems() {
+  let count = 0;
+
+  async function getKeys() {
+    return new Promise(async (resolve, reject) => {
+      // await delay(1000)
+      const ctrlKeylist = Array.from(
+        document.querySelectorAll(".bpx-player-ctrl-playbackrate-menu li"),
+      );
+      if (ctrlKeylist.length === 0) {
+        if (count <= 20) {
+          count += 1;
+          await delay(1000);
+          return getKeys();
+        } else {
+          reject("get ctrl keys error ");
+        }
+      } else {
+        resolve(ctrlKeylist);
+      }
+    });
+  }
+
+  const keys = await getKeys();
+  return keys;
+}
+
+async function delay(time) {
+  return new Promise((res) => setTimeout(() => res(), time));
+}
