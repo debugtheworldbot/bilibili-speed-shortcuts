7af0f0cea96ce16618b0f84549473d059b387d0a
tian <istiancz@gmail.com>
2023-10-10 20:17:33 +0800

    feat: add setting pannel

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         bilibili 倍速控制
 // @namespace    http://tampermonkey.net/
-// @version      0.8
+// @version      0.9
 // @description  使用快捷键控制bilibili的倍速选择
 // @author       pipizhu
 // @match        http*://www.bilibili.com/video/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
-// @grant        none
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
 // @license 		 MIT
 // ==/UserScript==
 
@@ -20,51 +22,20 @@ const speedList = [0.5, 0.75, 1, 1.25, 1.5, 2];
 
 let upKeyCode = "Period";
 let downKeyCode = "Comma";
-function addActionBtns() {
-  const videoArea = document.querySelector(".bpx-player-primary-area");
-  const button = document.createElement("button");
-  button.innerHTML = "倍速";
-  button.style = "cursor: pointer;position: absolute;right: -30px;bottom: 0;";
-  button.addEventListener("click", () => {
-    const div = document.createElement("div");
-    const downInput = document.createElement("input");
-    const upInput = document.createElement("input");
-    const saveBtn = document.createElement("button");
-    downInput.value = downKeyCode;
-    upInput.value = upKeyCode;
-    downInput.maxLength = upInput.maxLength = 1;
-    downInput.style = "width: 1rem";
-    upInput.style = "width: 1rem";
-    div.style.position = "absolute";
-    div.style.right = "0";
-    div.style.bottom = "0";
-    div.style.background = "white";
-    div.style.zIndex = "1000";
-    div.append("倍速+");
-    div.appendChild(downInput);
-    div.append("倍速-");
-    div.appendChild(upInput);
-    div.appendChild(saveBtn);
-    videoArea.appendChild(div);
-    saveBtn.innerHTML = "保存";
-    saveBtn.addEventListener("click", () => {
-      upKeyCode = upInput.value;
-      downKeyCode = downInput.value;
-      div.innerHTML = "保存成功";
-      setTimeout(() => {
-        div.remove();
-      }, 3000);
-    });
-  });
-  videoArea.appendChild(button);
-}
 
 async function init() {
   // addActionBtns();
   console.log("init keybindings");
+  GM_registerMenuCommand("更多设置", openSettings);
   try {
+    const cachedSpeed = GM_getValue("currentSpeed");
     // await delay(1000);
     speedOptions = await initKeyElems();
+    if (cachedSpeed) {
+      console.log("cached speed", cachedSpeed);
+      GM_registerMenuCommand("✅当前记忆倍速 X" + cachedSpeed, () => { });
+      setSpeed(cachedSpeed);
+    }
     bindKeys();
     console.log("bind success!");
   } catch (e) {
@@ -166,4 +137,66 @@ function getCurrentVideo() {
   if (vdos.length > 0) return vdos;
   vdos = document.querySelectorAll(".bpx-player-video-wrap video");
   return vdos[0];
+}
+
+function addStyle(styleString) {
+  const style = document.createElement("style");
+  style.type = "text/css";
+  style.appendChild(document.createTextNode(styleString));
+  document.head.append(style);
+}
+
+function preserveSpeed() {
+  const currentSpeed = getCurrentSpeed();
+  GM_setValue("currentSpeed", currentSpeed);
+}
+
+function clearSpeed() {
+  GM_setValue("currentSpeed", "");
+}
+
+function openSettings() {
+  const speed = getCurrentSpeed();
+  const html = `
+    <div class='bli-speed-pannel-setting' id='bli-speed-pannel-setting' style='position: fixed; bottom: 0; right: 0;top: 0;left:0; z-index: 9999;display:flex;justify-content: center;align-items: center'>
+      <div style='position:absolute; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5);z-index: -1'></div>
+      <div style='background: white;padding:20px;border-radius: 5px;'>
+        <h2 style='text-align: center;'>当前速度：${speed}</h2>
+        <div>
+          <button id='save-speed'> 记忆当前速度 </button>
+          <button id='clear-speed'> 取消记忆当前速度 </button>
+        </div>
+        <button style='display: block; margin: 0 auto;margin-top: 20px; ' id='close-pannel-setting'>关闭面板</button>
+      </div>
+    </div>
+  `;
+  document.body.insertAdjacentHTML("beforeend", html);
+  addStyle(`
+  .bli-speed-pannel-setting button{
+      background: transparent;
+      border: 1px solid;
+      padding: 5px 8px;
+      border-radius: 25px;
+    }
+  `);
+
+  const closePannel = () => {
+    document.body.removeChild(
+      document.getElementById("bli-speed-pannel-setting"),
+    );
+  };
+
+  document.getElementById("save-speed").addEventListener("click", () => {
+    preserveSpeed();
+    closePannel();
+  });
+  document.getElementById("clear-speed").addEventListener("click", () => {
+    clearSpeed();
+    closePannel();
+  });
+  document
+    .getElementById("close-pannel-setting")
+    .addEventListener("click", () => {
+      closePannel();
+    });
 }
